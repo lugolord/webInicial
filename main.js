3f3c4dbf8634babb90c5c6e3a980a248de946590
luis <lugo50635@gmail.comm>
2022-05-11 16:18:16 -0300

    introduccion a JS

---
+++ main.js
@@ -0,0 +1,112 @@
+/* VARIABLES */
+
+// var username = '23';
+
+// username = 31345;
+
+// let userAge = 30.3;
+
+// userAge = 45;
+
+// const pi = 3.14;
+
+// // pi = 4.14;
+
+
+
+// TIPOS DE DATOS
+
+// let product = 'iPhone'; // string
+
+// let stock = 10.234; // number
+
+// let darkMode = false; // boolean
+
+// let dontKnow; // undefined
+
+// let empty = null; // null
+
+// let notANumber = NaN; // NaN
+
+
+
+// CONSOLA
+
+// console.log('Hello world!');
+
+// console.log(darkMode);
+
+// console.log(product, stock);
+
+// console.log(dontKnow);
+
+
+
+// PROMPT Y ALERT
+
+// let userBirth = prompt('Hola! Por favor, ingrese su año de nacimiento');
+
+// console.log(userBirth);
+
+// alert(userBirth);
+
+
+
+// OPERACIONES NUMERICAS
+
+// let sum = 3 + 2; // 5
+
+// console.log(3 + 3); //6
+
+// let subtraction = 13 - 4; // 9
+
+// let multiplication = 2 * 7; // 14
+
+// let division = 9 / 3; // 3
+
+// let resto = 10 % 2; // 1
+
+
+
+// OPERACIONES PARA STRINGS
+
+// let pika = 'pika';
+
+// let chu = 'chu';
+
+// let pikachu = 'pika' + 'chu';
+
+// let myName = prompt('Ingresa tu nombre por favor');
+
+// let otroName = prompt('Ingrese otro nombre');
+
+// alert('Hola ' + myName + ' y ' + otroName + ' bienvenidos!');
+
+
+
+// CURIOSIDADES
+
+// let primeraCuriosidad = 2 + '2';
+
+// console.log(primeraCuriosidad);
+
+// let segundaCuriosidad = 2 - '2';
+
+// console.log(segundaCuriosidad);
+
+// let terceraCuriosidad = 2 * '2';
+
+// console.log(terceraCuriosidad);
+
+
+
+// PROBLEMA PROMPT 
+
+// let problema = prompt('Ingrese un numero');
+
+// alert(problema + 3);
+
+
+// let solucion = parseInt(problema);
+
+// alert(solucion + 3);
